5477473b61e9b754c5191ca2e1dcb263885f3080
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2025-03-25 15:40:26 +0100

    Add project

---
+++ README.md
@@ -0,0 +1,12 @@
+# tetris
+Clone of the Tetris game with some modifications.
+
+<p><img src="https://github.com/ArtCC/tetris/blob/main/Screenshots/game.png" width="750"></p>
+
+## License
+
+[Apache License](LICENSE)
+
+---
+
+**Arturo Carretero Calvo - 2025**
---
+++ tetris/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  tetris
+//
+//  Created by Arturo Carretero Calvo on 25/3/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+}
